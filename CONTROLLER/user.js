28bd5b8bbc51aa9389a66c39e2e84c077abf049f
Noor Fatima <noor2239064@gmail.com>
2026-07-13 22:37:07 +0500

    created delete and update

--- CONTROLLER/user.js
+++ CONTROLLER/user.js
@@ -1,3 +1,4 @@
+const { model } = require('mongoose');
 const User = require('../MODEL/user');
 
 const createUsers = async (req,res)=>{
@@ -11,5 +12,23 @@ const getUsers = async (req,res)=>{
     res.send(data);
 }
 
+const getUser = async (req, res)=>{
+    const singleUser = await User.findById(req.params.id);
+    res.send(singleUser);
+}
 
-module.exports ={ createUsers , getUsers }
+const deleteUser = async (req,res)=>{
+    const deletedUser = await User.findByIdAndDelete(req.params.id);
+    console.log("User has been deleted");
+}
+
+const updateUser = async (req,res)=>{
+    const id = req.params.id;
+    const body = req.body;
+    const updatedUser = await User.findByIdAndUpdate(id,body, {new:true});
+    res.send(updatedUser);
+}
+
+
+
+module.exports ={ createUsers , getUsers, getUser, deleteUser, updateUser};
--- VIEW/user.js
+++ VIEW/user.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const Router = express.Router();
-const { createUsers , getUsers } = require('../CONTROLLER/user');
+const { createUsers, getUsers, getUser, deleteUser, updateUser } = require('../CONTROLLER/user');
 
 Router.post('/createUsers', createUsers);
 Router.get('/getUsers', getUsers);
+Router.get('/getUser/:id', getUser);
+Router.patch('/getUser/:id', updateUser);
+Router.delete('/getUser/:id', deleteUser);
 
-module.exports = Router;
+module.exports = Router;  
